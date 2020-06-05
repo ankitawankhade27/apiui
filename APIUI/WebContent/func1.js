2e8442236eda4cfc9b33c874a03089f850340ef2
ankitawankhade27 <ankitawankhade27@gmail.com>
2020-06-05 23:00:13 +0530

    updated

--- APIUI/WebContent/func1.js
+++ APIUI/WebContent/func1.js
@@ -1,3 +1,52 @@
+
+//update Employee
+$(document).ready(
+
+		
+		function()
+		{
+			//Show Employee List to be updated
+			$("#updateEmployee").click(
+			
+					function ()
+					{
+						alert("Employee updated Successfully ....");
+						$.ajax(
+						{
+							type: "PUT",
+							contentType : "application/json",
+							url : "http://localhost:9090/apii/updatecountrynamehb1",
+							dataType : "json",
+							data : formTojson()
+						}		
+						).then
+						(
+								
+						);						
+					}
+			);
+			
+			function formTojson()
+			{
+				return JSON.stringify(
+						{
+							"name": $('#name').val(), 
+							"phoneno": $('#phoneno').val(), 
+							"department": $('#department').val(), 
+							"status": $('#status').val(),
+							"createddtm": $('#createddt').val(), 
+							"createdby": $('#createdby').val(), 
+							"updateddtm": $('#updateddt').val(), 
+							"updatedby": $('#updatedby').val(), 
+							"cid": $('#cid').val(), 
+							"cname": $('#cname').val()
+						}  
+				)
+			}
+		}
+);
+
+//update country
 $(document).ready(
 
 		
@@ -35,3 +84,4 @@ $(document).ready(
 			}
 		}
 );
+
